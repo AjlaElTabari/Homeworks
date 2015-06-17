ea8b112d3c9865c6b7624e17229f279b122cf3b5
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-06-17 23:13:20 +0200

    Solved Task01 and Task02 for Homework 15

---
+++ homework15/src/ba/bitcamp/homework15/cashebox/Main.java
@@ -0,0 +1,26 @@
+package ba.bitcamp.homework15.cashebox;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Declaring and initializing new Cash box
+		CashBox cb1 = new CashBox("MySavings");
+		// Printing information about the Cash box
+		cb1.printInformation();
+		
+		// Adding some money
+		cb1.addMoney(20);
+		cb1.printInformation();
+
+		cb1.addMoney(30);
+		cb1.printInformation();
+		
+		cb1.addMoney(100);
+		cb1.printInformation();
+		
+		// Taking everything
+		cb1.takeAllMoney();
+		cb1.printInformation();
+	}
+
+}
