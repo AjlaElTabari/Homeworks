b7651b9c00ed163ffb38ee148df80cd20e3031f0
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-06-25 00:01:34 +0200

    Solved tasks for homework_oop

---
+++ homework_oop/src/homework_oop/StarNetwork.java
@@ -0,0 +1,78 @@
+package homework_oop;
+
+public class StarNetwork extends Network implements Functionable {
+	private Server server;
+	
+	public StarNetwork(String name, Server server) {
+		super(name);
+		this.server = server;
+	}
+
+	/**
+	 * Add computer in the computer array network.
+	 * @param pc - Computer that needs to be added.
+	 */
+	public void addComputer(Computer computer) {
+
+		Client c = (Client) computer;
+
+		Computer[] computers = new Computer[getComputers().length + 1];
+
+		// Checks if the network is empty
+		if (getComputers()[0] == null) {
+			computers[0] = c;
+			c.connect(this);
+		} else {
+			System.arraycopy(getComputers(), 0, computers, 0, getComputers().length);
+			computers[computers.length - 1] = c;
+			c.connect(this);
+			setComputers(computers);
+		}
+	}
+
+	/**
+	 * Remove computer from the computer network.
+	 * @param pc - Computer that needs to be removed.
+	 */
+	public void removeComputer(Computer computer) {
+
+		Client c = (Client) computer;
+
+		for (int i = 0; i < getComputers().length; i++) {
+			if (c.equals(getComputers()[i])) {
+				c.disconnect();
+				getComputers()[i] = null;
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		Computer[] computers = new Computer[getComputers().length - 1];
+
+		for (int i = 0; i < computers.length; i++) {
+			if (getComputers()[i] != null) {
+				computers[i] = getComputers()[i];
+			}
+		}
+		setComputers(computers);
+	}
+	
+	/**
+	 * Checks if the network functioning
+	 * Network is functioning if number of connected computers 
+	 * is smaller then max capacity of server
+	 */
+	public boolean isFunctioning() {
+		int counter = 0;
+
+		for (int i = 0; i < getComputers().length; i++) {
+			if (getComputers()[i] != null) {
+				counter++;
+			}
+		}
+		if (counter < server.getCapacity()) {
+			return true;
+		}
+		return false;
+	}
+}
