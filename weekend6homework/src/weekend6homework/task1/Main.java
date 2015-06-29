19889c60e68745bf3818a9fe60b29c964d9bf07b
AjlaElTabari <ajla.eltabari@bitcamp.ba>
2015-06-29 20:13:07 +0200

    Tasks were in wrong packages

---
+++ weekend6homework/src/weekend6homework/task1/Main.java
@@ -0,0 +1,86 @@
+package weekend6homework.task1;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ * Creates GUI application that us a window with a row of buttons, 
+ * named with numbers from 0 to 9. User is allowed to pick up (by pressing buttons)
+ * three numbers. After that, to user will be presented another window with printed 
+ * out three digits number formed from selected numbers.
+ * @author ajla
+ *
+ */
+public class Main extends JFrame {
+
+	private static final long serialVersionUID = 993668091274336134L;
+	private static final int PICKED_NUMBERS_COUNTER = 1;
+	private static final int MAX_NUMBERS = 3;
+	private static final int NUMBER_OF_BUTTONS = 10;
+	private JButton[] buttons = new JButton[NUMBER_OF_BUTTONS];
+	private String res = "";
+	private int counter = 1;
+
+	public Main() {
+		setLayout(new GridLayout(10, 1));
+		// Creating buttons and label
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton((i) + "");
+			buttons[i].setFont(new Font("Cambria", Font.BOLD, 30));
+			buttons[i].addActionListener(new Action());
+			add(buttons[i]);
+		}
+
+		// Setting the layout
+		setResizable(false);
+		setTitle("Task1");
+		setSize(200, 700);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	// Creates new instance of our GUI app
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	/**
+	 * Creating a class named Action that implements ActionListener interface.
+	 * It allows us to create buttons behviour. 
+	 * There is an array of 10 buttons. User is allowed to pick up three numbers
+	 * (by pressing buttons). After that, another window will be opened, presenting to the user
+	 * three digits number, formed from picked numbers
+	 * @author ajla
+	 *
+	 */	
+	private class Action implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for (int i = 0; i < buttons.length; i++) {
+				if (e.getSource() == buttons[i]) {
+					if (counter == MAX_NUMBERS) {
+						res += buttons[i].getText();
+						JOptionPane.showMessageDialog(null, "Number " + res);
+						System.exit(EXIT_ON_CLOSE);
+						counter = PICKED_NUMBERS_COUNTER;
+						res = "";
+						buttons[i].setEnabled(false);
+					} else {
+						res += buttons[i].getText();
+						counter++;
+						buttons[i].setEnabled(false);
+					}
+
+				}
+			}
+		}
+
+	}
+
+}
---
+++ weekend6homework/src/weekend6homework/task4/Main.java
@@ -0,0 +1,134 @@
+package weekend6homework.task4;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+ /** Creates GUI application that represents us a window with four buttons
+ * and one label. Buttons allows us to insert characters (shown in label after that)
+ * and to iterate through the array of characters.
+ * Also, we are able to insert additional characters during iteration.
+ * @author ajla
+ *
+ */
+public class Main extends JFrame {
+
+	// Creating buttons and label
+	private static final long serialVersionUID = 3509159316291937485L;
+
+	private static final char POSITIONER = '_';
+	private JButton buttonGoLeft = new JButton("<");
+	private JLabel chars = new JLabel(Character.toString(POSITIONER));
+	private JButton buttonGoRight = new JButton(">");
+	private JButton buttonChar = new JButton("Generate char");
+	private JButton buttonInsert = new JButton("Insert");
+	private String characters = "";
+
+	/**
+	 * Constructor Creates a window with belonging buttons.
+	 * Adds ActionListener to the buttons.
+	 */
+	public Main() {
+
+		setLayout(new BorderLayout());
+		// Setting buttons
+		add(buttonGoLeft, BorderLayout.WEST);
+		add(chars, BorderLayout.CENTER);
+		add(buttonGoRight, BorderLayout.EAST);
+		add(buttonChar, BorderLayout.NORTH);
+		add(buttonInsert, BorderLayout.SOUTH);
+
+		// Adding action listener to the buttons
+		buttonGoLeft.addActionListener(new Action());
+		buttonGoRight.addActionListener(new Action());
+		buttonChar.addActionListener(new Action());
+		buttonInsert.addActionListener(new Action());
+
+		chars.setHorizontalAlignment(JLabel.CENTER);
+
+		// Setting the layout
+		setResizable(false);
+		setTitle("Task4");
+		setSize(500, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	/**
+	 * Creating a class named Action that implements ActionListener interface.
+	 * It allows us to create buttons behviour. 
+	 * There are four buttons: 
+	 * buttonChar - generates random lowercase character every time it is pressed,
+	 * buttonGoRight - moves position character "_" by one place to the right,
+	 * buttonGoLeft - moves position character "_" by one place to the left, and
+	 * buttonInsert - inserts earlier randomly generated character to the place marked with positioner "_"
+	 * @author ajla
+	 *
+	 */
+	private class Action implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			// randomly generate one lowercase character
+			Random r = new Random();
+			char newChar = (char) (r.nextInt(26) + 'a');
+
+			// if buttonChar is pressed, early generated 
+			// random character is shown as buttonChar text
+			if (e.getSource() == buttonChar) {
+				buttonChar.setText(Character.toString(newChar));
+			}
+			// if buttonInsert is pressed, earlier randomly generated
+			// character will be inserted to the place marked with positioner 
+			else if (e.getSource() == buttonInsert) {
+				int index = chars.getText().indexOf(POSITIONER);
+				characters = chars.getText().substring(0, index);
+				characters += buttonChar.getText() + POSITIONER;
+				characters += chars.getText().substring(index + 1,
+						chars.getText().length());
+				chars.setText(characters);
+			} 
+			// if buttonGoLeft is pressed, positioner will be moved by one place to the left
+			else if (e.getSource() == buttonGoLeft) {
+				String newCharacters = "";
+				int index = chars.getText().indexOf(POSITIONER);
+
+				if (index > 0) {
+					index--;
+					newCharacters = characters.substring(0, index);
+					newCharacters += POSITIONER;
+					newCharacters += characters.substring(index,
+							characters.length() - 1);
+
+					chars.setText(newCharacters);
+				}
+			} 
+			// if buttonGoRight is pressed, positioner will be moved by one place to the right
+			else if (e.getSource() == buttonGoRight) {
+				String newCharacters = "";
+				int index = chars.getText().indexOf(POSITIONER);
+				if (index < characters.length()) {
+					index++;
+					newCharacters = characters.substring(0, index);
+					newCharacters += POSITIONER;
+					if (index != characters.length()) {
+						newCharacters += characters.substring(index,
+								characters.length() - 1);
+						chars.setText(newCharacters);
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// Creates new instance of our GUI app
+		new Main();
+	}
+
+}
